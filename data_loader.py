c39d8bb7b9ea4b4df3f7c9120255a8e4ee0dbe36
‘manjusaka-L’ <2847289449@qq.com>
2024-04-14 20:27:03 +0800

    data loader

--- data_loader.py
+++ data_loader.py
@@ -18,8 +18,8 @@ class Data_loader():
 
         elif dataset == 'amazon':
             self.path_for_whole = 'data/amazon-book/amazon-books-enc.csv'
-            self.path_for_train = './amazon-book/train_amazon%s.dat' % version
-            self.path_for_test = './amazon-book/test_amazon%s.dat' % version
+            self.path_for_train = 'data/amazon-book/train_amazon%s.dat' % version
+            self.path_for_test = 'data/amazon-book/test_amazon%s.dat' % version
             # self.path_for_train='./amazon-book/train_amazon%s.dat.1k'%(version)
             # self.path_for_test='./amazon-book/test_amazon%s.dat.1w'%(version)
             self.num_u = 35736
@@ -27,8 +27,8 @@ class Data_loader():
 
         elif dataset == 'yelp':
             self.path_for_whole = 'data/yelp/YELP_encoded.csv'
-            self.path_for_train = './yelp/train_yelp%s.dat' % version
-            self.path_for_test = './yelp/test_yelp%s.dat' % version
+            self.path_for_train = 'data/yelp/train_yelp%s.dat' % version
+            self.path_for_test = 'data/yelp/test_yelp%s.dat' % version
             # self.path_for_test='./yelp/test_yelp%s.dat.1w'%(version)
             self.num_u = 41772
             self.num_v = 30037
@@ -36,8 +36,8 @@ class Data_loader():
         elif dataset == 'beauty':
             #  interactions 172,188
             self.path_for_whole = 'data/beauty/beauty.csv'
-            self.path_for_train = './beauty/train_beauty%s.dat' % version
-            self.path_for_test = './beauty/test_beauty%s.dat' % version
+            self.path_for_train = 'data/beauty/train_beauty%s.dat' % version
+            self.path_for_test = 'data/beauty/test_beauty%s.dat' % version
             self.num_u = 22363
             self.num_v = 12101
 
